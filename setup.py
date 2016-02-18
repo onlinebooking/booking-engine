a08a85b02a04a30b1cfd7054909c3ac74f916030
Mauro Bianchi <bianchimro@gmail.com>
2016-02-18 12:51:03 +0100

    find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='booking_engine',
     version='0.1',
-    packages=['booking_engine',],
+    packages=find_packages(),
     license='MIT',
     long_description="Booking engine",
     tests_require=['nose>=1.3.7', 'coverage>=4.0.3'],
